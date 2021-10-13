a4128570d501f3bbeb9dac69696d5989cbe88e9e
Michael Waddell <michael@waddellnet.com>
2021-10-13 16:22:34 -0500

    upgrading rspec

--- lib/redmine_plugin_support/rspec_task.rb
+++ lib/redmine_plugin_support/rspec_task.rb
@@ -1,43 +1,41 @@
 gem 'rspec'
 gem 'rspec-rails'
-require 'spec/rake/spectask'
+require 'rspec/core/rake_task'
 
 module RedminePluginSupport
   class RspecTask < GeneralTask
     def define
 
       desc "Run all specs in spec directory (excluding plugin specs)"
-      Spec::Rake::SpecTask.new(:spec) do |t|
-        t.spec_opts = ['--options', "\"#{RedmineHelper.plugin_root}/spec/spec.opts\""]
-        t.spec_files = FileList['spec/**/*_spec.rb']
+      RSpec::Core::RakeTask.new(:spec) do |t|
+        t.rspec_opts = ['-c', '-f progress', "-r \"#{RedmineHelper.plugin_root}/spec/spec_helper.rb\""]
+        t.pattern = 'spec/**/*_spec.rb'
       end
       
       namespace :spec do
         desc "Run all specs in spec directory with RCov (excluding plugin specs)"
-        Spec::Rake::SpecTask.new(:rcov) do |t|
-          t.spec_opts = ['--options', "\"#{RedmineHelper.plugin_root}/spec/spec.opts\""]
-          t.spec_files = FileList['spec/**/*_spec.rb']
+        RSpec::Core::RakeTask.new(:rcov) do |t|
+          t.rspec_opts = ['-c', '-f progress', "-r \"#{RedmineHelper.plugin_root}/spec/spec_helper.rb\""]
+          t.pattern = 'spec/**/*_spec.rb'
           t.rcov = true
           t.rcov_opts << ["--rails", "--sort=coverage", "--exclude '/var/lib/gems,spec,#{RedmineHelper.redmine_app},#{RedmineHelper.redmine_lib}'"]
         end
         
         desc "Print Specdoc for all specs (excluding plugin specs)"
-        Spec::Rake::SpecTask.new(:doc) do |t|
-          t.spec_opts = ["--format", "specdoc", "--dry-run"]
-          t.spec_files = FileList['spec/**/*_spec.rb']
+        RSpec::Core::RakeTask.new(:doc) do |t|
+          t.pattern = 'spec/**/*_spec.rb'
         end
 
         desc "Print Specdoc for all specs as HTML (excluding plugin specs)"
-        Spec::Rake::SpecTask.new(:htmldoc) do |t|
-          t.spec_opts = ["--format", "html:doc/rspec_report.html", "--loadby", "mtime"]
-          t.spec_files = FileList['spec/**/*_spec.rb']
+        RSpec::Core::RakeTask.new(:htmldoc) do |t|
+          t.pattern = 'spec/**/*_spec.rb'
         end
 
         [:models, :controllers, :views, :helpers, :lib].each do |sub|
           desc "Run the specs under spec/#{sub}"
-          Spec::Rake::SpecTask.new(sub) do |t|
-            t.spec_opts = ['--options', "\"#{RedmineHelper.plugin_root}/spec/spec.opts\""]
-            t.spec_files = FileList["spec/#{sub}/**/*_spec.rb"]
+          RSpec::Core::RakeTask.new(sub) do |t|
+            t.rspec_opts = ['-c', '-f progress', "-r \"#{RedmineHelper.plugin_root}/spec/spec_helper.rb\""]
+            t.pattern = "spec/#{sub}/**/*_spec.rb"
           end
         end
       end
--- tasks/rspec.rake
+++ tasks/rspec.rake
@@ -5,7 +5,7 @@ rescue LoadError
   require 'spec'
 end
 begin
-  require 'spec/rake/spectask'
+  require 'rspec/core/rake_task'
 rescue LoadError
   puts <<-EOS
 To use rspec for testing you must install rspec gem:
@@ -15,7 +15,7 @@ EOS
 end
 
 desc "Run the specs under spec/models"
-Spec::Rake::SpecTask.new do |t|
-  t.spec_opts = ['--options', "spec/spec.opts"]
-  t.spec_files = FileList['spec/**/*_spec.rb']
+RSpec::Core::RakeTask.new do |t|
+  t.rspec_opts = ["-c", "-f progress", "-r ./spec/spec_helper.rb"]
+  t.pattern = 'spec/**/*_spec.rb'
 end
